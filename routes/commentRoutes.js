b14021a3678c0be92845f69a6f0955021f52874c
ramuduyelisetti <131511389+ramuduyelisetti@users.noreply.github.com>
2024-02-08 23:54:58 +0530

    commentRoutes.js

---
+++ routes/commentRoutes.js
@@ -0,0 +1,8 @@
+// routes/commentRoutes.js
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/Comment');
+
+// Define routes for commenting on posts, replying to comments, etc.
+
+module.exports = router;
