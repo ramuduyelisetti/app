60883e917cde725ad82a72f09318496b6dcc300a
ramuduyelisetti <131511389+ramuduyelisetti@users.noreply.github.com>
2024-02-08 23:56:33 +0530

    postRoutes.js

---
+++ routes/postRoutes.js
@@ -0,0 +1,8 @@
+// routes/postRoutes.js
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Define routes for posting posts for discussion, etc.
+
+module.exports = router;
