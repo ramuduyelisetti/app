1b198162d0aaa114505d42adc0c9742836ab5ac0
ramuduyelisetti <131511389+ramuduyelisetti@users.noreply.github.com>
2024-02-08 23:57:15 +0530

    userRoutes.js

---
+++ routes/routes/userRoutes.js
@@ -0,0 +1,8 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Define routes for user registration, login, etc.
+
+module.exports = router;
